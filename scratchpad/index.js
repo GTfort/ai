369e548dc07aa94be589f6f8612f96c5ce22a6b2
Itode fortune <fortune.nwuneke@gmail.com>
2025-01-08 00:59:24 +0100

    testing embedding

--- scratchpad/index.js
+++ scratchpad/index.js
@@ -2,4 +2,13 @@ require("dotenv").config();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+const model = genAI.getGenerativeModel({ model: "text-embedding-004" });
+
+async function run() {
+  const text = "THE DOG RAN OVER THE LAZY FOX";
+  const result = await model.embedContent(text);
+  const embedding = result.embedding;
+  console.log("Embedding: ", embedding);
+}
+
+run();
